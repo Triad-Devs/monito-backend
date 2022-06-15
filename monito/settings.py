f3baf4510f6b40e6c7d7fb138f526db59b79fb4c
Viraj Patidar <vrj381@gmail.com>
2022-06-15 21:43:51 +0530

    basic API

--- monito/settings.py
+++ monito/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 from decouple import config
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -39,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'monito_api',
     'corsheaders',
 
     'rest_framework',
@@ -128,6 +130,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+MEDIA_URL = '/media/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
---
+++ monito_api/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class MonitoApiConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'monito_api'
