a718bc88fbc55b4c79b3e6a74a706fe1e11687a1
Viraj Patidar <vrj381@gmail.com>
2022-06-19 13:54:23 +0530

    monitoring models

--- monito_api/admin.py
+++ monito_api/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Moniurl, Log
 
 # Register your models here.
+
+admin.site.register(Moniurl)
+admin.site.register(Log)
--- monito_api/models.py
+++ monito_api/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+
+class Moniurl(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user')
+    description = models.CharField(max_length=100, blank=True)
+    isAPI = models.BooleanField()
+    repeatAfter = models.IntegerField()
+    httpMethod = models.CharField(max_length=10)
+    url = models.CharField(max_length=100)
+    authReq = models.BooleanField()
+    bearer = models.CharField(max_length=100, blank=True)
+    entered_on = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user}_____{self.httpMethod}_____{self.url}"
+
+
+class Log(models.Model):
+    url = models.ForeignKey(Moniurl, on_delete=models.CASCADE, related_name='moni_url')
+    req_url = models.CharField(max_length=100)
+    status_code = models.IntegerField()
+    content_length = models.IntegerField()
+    time_taken = models.DecimalField(max_digits=19, decimal_places=7)
+    content_type = models.CharField(max_length=30)
+
+
+
+    def __str__(self):
+        return f"{self.url}_____{self.status_code}_____{self.req_url}"
