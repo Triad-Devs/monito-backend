d3634de1997a82f39bfa46468ba622b18acda7b4
Viraj Patidar <vrj381@gmail.com>
2022-06-19 17:51:45 +0530

    testing task args

--- monito_api/views.py
+++ monito_api/views.py
@@ -1,10 +1,22 @@
 from django.shortcuts import render
+
 from django_celery_beat.models import PeriodicTask, CrontabSchedule, IntervalSchedule
+
 import json
 import random
+from datetime import datetime
 
 from rest_framework import generics, status
+from rest_framework import permissions
 from rest_framework.response import Response
+
+from .serializers import (
+    NewURLSerializer
+)
+
+from django.core.exceptions import ObjectDoesNotExist
+
+
 
 # Create your views here.
 
@@ -15,5 +27,8 @@ class TestView(generics.GenericAPIView):
             every=3,
             period=IntervalSchedule.MINUTES,
         )
-        task = PeriodicTask.objects.create(interval=schedule, name="send_request_task_"+str(random.randint(3,100)), task='monito_api.tasks.send_request_func', args=json.dumps(['https://api.zippopotam.us/us/33162']))
+        task = PeriodicTask.objects.create(interval=schedule, name="send_request_task_"+str(random.randint(3,100)), task='monito_api.tasks.send_request_func', args=json.dumps([1, 'https://jsonplaceholder.typicode.com/todos/1', 'GET', {}, ""]))
         return Response({'resp': "It's Working"}, status=status.HTTP_200_OK)
+
+
+
