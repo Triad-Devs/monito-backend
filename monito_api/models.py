3eec0b8fcf5147f4255bcbf4c997fdcf2d6a4a93
Viraj Patidar <vrj381@gmail.com>
2022-06-20 09:40:23 +0530

    JSON body stored in DB

--- monito_api/models.py
+++ monito_api/models.py
@@ -5,15 +5,22 @@ from django.contrib.auth.models import User
 
 
 class Moniurl(models.Model):
+    
+    def JSON_default():
+        return {}
+
+        
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user')
     description = models.CharField(max_length=100, blank=True)
     isAPI = models.BooleanField()
     repeatAfter = models.IntegerField()
     httpMethod = models.CharField(max_length=10)
     url = models.CharField(max_length=100)
+    JSONbody = models.JSONField(default=JSON_default)
     authReq = models.BooleanField()
     bearer = models.CharField(max_length=100, blank=True)
     entered_on = models.DateTimeField(auto_now_add=True)
+
 
     def __str__(self):
         return f"{self.user}__{self.httpMethod}__{self.url}"
