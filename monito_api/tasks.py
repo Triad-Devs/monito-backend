59c70441a4099befd87c846602af7a6c038452af
atharvadpatil <atharva.dp@somaiya.edu>
2022-07-22 20:13:47 +0530

    trigger alert notification logic

--- monito_api/tasks.py
+++ monito_api/tasks.py
@@ -3,7 +3,7 @@ import requests
 from requests.structures import CaseInsensitiveDict
 from datetime import datetime
 from .models import Log, Moniurl
-
+from .utils import Util
 
 @shared_task(bind=True)
 def send_request_func(self, url_id, url, httpMethod, JSONbody, bearer):
@@ -31,14 +31,31 @@ def send_request_func(self, url_id, url, httpMethod, JSONbody, bearer):
     if httpMethod == 'PATCH':
         r = requests.patch(url, headers=headers, data=JSONbody)
 
-
     if httpMethod == 'DELETE':
         r = requests.delete(url, headers=headers)
 
 
+    url = Moniurl.objects.get(pk=url_id) 
+
+    if r.status_code >= 400 and r.status_code <=599:
+        url.failedCount +=1
+        url.save()
+        if url.failedCount >= url.alertThreshold:
+            email_body = 'Hi '+ url.user.first_name +',\n\nWe have noticed some irregularities in your URL.\nBelow are the details:\nURL: '+url.url+'\nMethod: '+url.httpMethod+'\nAlert Threshold: '+str(url.alertThreshold)+'\n\nRegards Monito,\nBy Team Triad'
+            to_email = url.user.email
+            data = {'email_body': email_body, 'to_email': to_email,
+                'email_subject': r'Monito Alert! {}'.format(url.url)}
+            Util.send_email(data)
+            url.failedCount = 0
+            url.save()
+    else:
+        url.failedCount = 0
+        url.save()
+
+
 
     log = Log(
-        url = Moniurl.objects.get(pk=url_id),
+        url = url,
         req_url = r.url,
         status_code = r.status_code,
         content_length = len(r.content),
